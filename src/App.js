ec286aea19a5f90a818e73706edc20f86712c98e
Koji Nakagawa <lightupthesky0627@gmail.com>
2019-07-14 21:43:22 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Docker Test. Como estas?
         </a>
       </header>
     </div>
